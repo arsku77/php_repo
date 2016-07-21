a4a265d9822073b2f01c70209307092938adac27
roman <steppehare@gmail.com>
2016-07-21 18:40:53 +0300

    023 Simple php output in helloworld.php

---
+++ helloworld.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf8">
+	<title>Hello World!!!</title>
+	
+</head>
+<body>
+	<?php echo "some text<br />";	?>
+	<?php echo "some text"."new text";	?><br />
+	<?php echo 5 + 78; ?>
+</body>
+</html>
