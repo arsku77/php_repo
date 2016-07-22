d62f6235ba364bf518365179a6607b0567e2d72c
roman <steppehare@gmail.com>
2016-07-22 08:47:39 +0300

    026 Variables basics

---
+++ variable.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<title>Variables</title>
+	</head>
+	<body>
+	<?php
+		$var1 = 10;
+		echo $var1;
+		echo "<br />";
+		$var1 = 100;
+		echo $var1;
+		$var2 = "<br />Hello PHP";
+		echo $var2;
+	?>
+	</body>
+</html>
