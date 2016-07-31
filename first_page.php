69603151334910688c6998a3425a4344fab702f6
roman <steppehare@gmail.com>
2016-07-31 10:39:24 +0300

    059 Urlencode and rawurlencode

--- first_page.php
+++ first_page.php
@@ -6,6 +6,7 @@
 	<body>
 		<?php $link_name = "Second page"; ?>
 		<?php $id = 5; ?>
-		<a href="second_page.php?id=<?php echo $id; ?>"><?php echo $link_name; ?></a>
+		<?php $company = "Johnson & Johnson"; ?>
+		<a href="second_page.php?id=<?php echo $id; ?>&company=<?php echo rawurlencode($company); ?>"><?php echo $link_name; ?></a>
 	</body>
 </html>
--- second_page.php
+++ second_page.php
@@ -4,13 +4,15 @@
 		<title>Second page</title>
 	</head>
 	<body>
-		<pre>
-			<?php
-				# print_r($_GET);
-				$id = $_GET['id'];
-				echo $id;
-				
-			?>
-		</pre>
+		<?php
+			# print_r($_GET);
+			$id = $_GET['id'];
+			echo $id;
+		?>
+		<br />
+		<?php
+			$company = $_GET['company'];
+			echo $company;
+		?>		
 	</body>
 </html>
