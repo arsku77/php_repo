ac146c1f9362764d66e650929938a679d2f11b7e
roman <steppehare@gmail.com>
2016-08-01 19:10:21 +0300

    061 Include external files in code

---
+++ includes.php
@@ -0,0 +1,12 @@
+<?php
+	include("included_function.php");
+	include("included_header.php");
+?>
+	The header has been included.
+	<br />
+	<?php 
+		echo hello("Everybody");
+	?>
+	<br />
+	</body>
+</html>
