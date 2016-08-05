b1f887cbdbcc00083ac6bd25d1edafc697ae5a84
roman <steppehare@gmail.com>
2016-08-05 19:03:08 +0300

    072 Single-page form with validations

--- validation_functions.php
+++ validation_functions.php
@@ -1,36 +1,45 @@
 
 <?php
-		# * presence
-		function has_presence($value) {
-			return isset($value) && $value !== ""; 
-		}
+	# * presence
+	function has_presence($value) {
+		return isset($value) && $value !== ""; 
+	}
 
-		# * string length	
-		function has_max_length($value, $max) {
-			return strlen($value) <= $max;
-		}
+	# * string length	
+	function has_max_length($value, $max) {
+		return strlen($value) <= $max;
+	}
 
-		# * inclusion in a set
-		function has_inclusion_in($value, $set) {
-			return in_array($value, $set);
-		}
+	# * inclusion in a set
+	function has_inclusion_in($value, $set) {
+		return in_array($value, $set);
+	}
 
-		function form_errors($errors_array = array()) {
-				$output = "";
-				if (!empty($errors_array)) {
-					$output .= "<div class=\"errors\">";
-					$output .= "Please fix the following errors:";
-					$output .= "<ul>";
-					foreach ($errors_array as $key => $error) {
-						$output .= "<li>".$error."</li>";
-					}
+	function validate_max_lengths($fields_with_max_lengths) {
+ 		global $errors;
+ 		# Using an assoc. array
+ 		foreach ($fields_with_max_lengths as $field => $max) {
+			$value = $_POST[$field];
+			if (!has_max_length($value, $max)) {
+				$errors[$field] = ucfirst($field)." is too long.";
+			}
+ 		}
+	}
 
-					$output .= "</ul>";
-					$output .= "</div>";
-				}
-
-				return $output;
+	function form_errors($errors_array = array()) {
+		$output = "";
+		if (!empty($errors_array)) {
+			$output .= "<div class=\"errors\">";
+			$output .= "Please fix the following errors:";
+			$output .= "<ul>";
+			foreach ($errors_array as $key => $error) {
+				$output .= "<li>".$error."</li>";
 			}
 
-		
+			$output .= "</ul>";
+			$output .= "</div>";
+		}
+
+		return $output;
+	}
 ?>
