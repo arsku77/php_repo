85d9ed784cbe03ebf2934140ae6bb4e866fc23e4
roman <steppehare@gmail.com>
2016-08-06 13:42:23 +0300

    075 Get cookies

--- cookies.php
+++ cookies.php
@@ -2,9 +2,10 @@
 	# REMEMBER: Setting cookies must be before *any* HTML output
 	# 			unless output buffering is turned on.
 	$name = "test";
-	$value = 45;
+	$value = "Python";
 	$expire = time() + (7*24*60*60);
 	setcookie($name, $value, $expire);	
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -12,5 +13,12 @@
 		<title>Cookies</title>
 	</head>
 	<body>
+	<!--pre-->
+		<?php
+			$test = isset($_COOKIE["test"]) ? $_COOKIE["test"] : "no cookies";
+			echo $test;
+		?>
+	<!--/pre-->
+
 	</body>
 </html>
