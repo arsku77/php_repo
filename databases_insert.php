b731aa06a07a46248ce77edfdf569c1997a45ac8
roman <steppehare@gmail.com>
2016-08-07 14:14:52 +0300

    089 MySQLi: insert into table

---
+++ databases_insert.php
@@ -0,0 +1,53 @@
+<?php
+	require_once("error_reporting_file.php");
+	# 1. Create a database connection
+	$dbhost = "localhost";
+	$dbuser = "widget_cms";
+	$dbpass = "secretpassword";
+	$dbname = "widget_corp";
+	$connection = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
+	# Test is connection occured
+	if (mysqli_connect_errno()) {
+		die("Database connection failed: ".
+			mysqli_connect_error().
+			" (".mysqli_connect_errno().")");
+	}
+?>
+<?php
+	# Often these are form values in $_POST
+	$menu_name = "Edit me";
+	$position = 4;
+	$visible = 1;
+	# 2. Perform database query
+	$query  = "INSERT INTO subjects ( ";
+	$query .= "menu_name, position, visible ";
+	$query .= ") VALUES ( ";
+	$query .= "'{$menu_name}', {$position}, {$visible}";
+	$query .= ")";
+	$result = mysqli_query($connection, $query);
+	# Test if there was a query error
+	if ($result) {
+		# Success
+		# redirect_to("some_page.php");
+		echo "Success!";
+	} else {
+		# Failure
+		# $message = "Subject creation failed";
+		die("Database query failed. ".mysqli_error($connection));
+	}
+	
+?>
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<title>Databases</title>
+	</head>
+	<body>
+		
+	</body>
+</html>
+
+<?php
+	# 5. Close database connection
+	mysqli_close($connection);
+?>
