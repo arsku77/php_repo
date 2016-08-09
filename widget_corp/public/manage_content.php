ad7f1d30cf342e0271ff37fc6f5e66eb339c1c18
roman <steppehare@gmail.com>
2016-08-09 20:35:26 +0300

    098 CMS: Reusable page assets

--- widget_corp/public/manage_content.php
+++ widget_corp/public/manage_content.php
@@ -1,11 +1,14 @@
-<!DOCTYPE html>
-<html lang="en">
-	<head>
-		<title>untitled</title>
-	</head>
-	<body>
-		<?php
-				
-		?>
-	</body>
-</html>
+<?php include("../includes/layouts/header.php"); ?>
+<?php require_once("../includes/functions.php"); ?>
+
+<div id="main">
+	<div id="navigation">
+		&nbsp;
+	</div>
+	<div id="page">
+		<h2>Manage Content</h2>
+
+	</div>
+</div>
+
+<?php include("../includes/layouts/footer.php"); ?>
